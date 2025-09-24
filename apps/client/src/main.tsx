25a0c5e07bd0af888cfd5f0168b8cd73a5fa6c06
Taylor Buchheit <tjbuchhe@asu.edu>
2025-09-23 21:54:34 -0700

    Revert Main to main

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,12 +1,12 @@
+import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Login from './components/Navbar/Login/Login.tsx';
 import EventMap from './components/Mapping/EventMap.tsx';
 import Layout from './Layout.tsx';
 import Signup from './components/Navbar/Signup/Signup.tsx';
-import { Provider } from 'react-redux';
 import store from './store/store.ts';
 import Welcome from './components/Welcome.tsx';
 import UserProfile from './components/UserProfile/UserProfile.tsx';
