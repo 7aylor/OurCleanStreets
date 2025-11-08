f709ef0480973fda9ab0f5a9de8d58056357e378
Taylor Buchheit <tjbuchhe@asu.edu>
2025-11-08 11:21:51 -0800

    prisma singleton

--- apps/server/src/utils/prisma.ts
+++ apps/server/src/utils/prisma.ts
@@ -1,7 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
+// Prisma client singleton to avoid flooding the connection pool
+let prisma: PrismaClient;
+
 export const getPrismaClient = () => {
-  return new PrismaClient({
-    datasources: { db: { url: process.env.OCS_DB } },
-  });
+  if (!prisma) {
+    prisma = new PrismaClient({
+      datasources: { db: { url: process.env.OCS_DB } },
+      log: ['error', 'warn'],
+    });
+  }
+  return prisma;
 };
